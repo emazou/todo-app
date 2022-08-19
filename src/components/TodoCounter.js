0fde0e9dab1480fae89ab4e3936fe6233bcb5270
emazou <estefania.mazou@gmail.com>
2022-08-18 23:51:05 -0500

    components added

---
+++ src/components/TodoCounter.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../styles/TodoCounter.css'
+export default function TodoCounter() {
+  return (
+    <>
+      <h2 className='TodoCounter-title'>Has completado 2 de 3 tareas</h2>
+    </>
+  );
+}
---
+++ src/components/TodoSearch.js
@@ -0,0 +1,8 @@
+import React from 'react'
+export default function TodoSearch() {
+  return (
+    <>
+      <input placeholder='Ceboola' />
+    </>
+  );
+}
