0fdf90ec524f61c89124634d69764706f7175b67
emazou <estefania.mazou@gmail.com>
2022-08-20 16:09:42 -0500

    imported styles and created classname

--- src/components/CreateTodoButton.js
+++ src/components/CreateTodoButton.js
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import '../styles/CreateTodoButton.css'
 export default function CreateTodoButton() {
     return (
         <>
-            <button>+</button>
+            <button class="CreateTodoButton add-btn">
+                <div class="add-icon"></div>
+                <div class="btn-txt">Agregar tarea</div>
+            </button>
         </>
     )
 }
