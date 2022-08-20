26f2d3d9771d74c1d28c2e6c031d693771deba0c
emazou <estefania.mazou@gmail.com>
2022-08-20 16:10:26 -0500

    imported styles and created classname

--- src/components/TodoCounter.js
+++ src/components/TodoCounter.js
@@ -3,7 +3,7 @@ import '../styles/TodoCounter.css'
 export default function TodoCounter() {
   return (
     <>
-      <h2 className='TodoCounter-title'>Has completado 2 de 3 tareas</h2>
+      <h2 className='TodoCounter'>Has completado 2 de 3 tareas</h2>
     </>
   );
 }
