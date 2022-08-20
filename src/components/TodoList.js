bc6b25322714dbffef25079aea38c6964cfea92a
emazou <estefania.mazou@gmail.com>
2022-08-20 16:09:09 -0500

    imported styles and created classname

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import '../styles/TodoList.css'
 export default function TodoList(props) {
     return (
         <>
-            <section>
+            <section className='TodoList-container'>
                 <ul>
                     {props.children}
                 </ul>
