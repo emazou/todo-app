53f5dd066d76c8081b2cc9e998858fb04dec2236
emazou <estefania.mazou@gmail.com>
2022-08-19 21:17:02 -0500

    homeJS imported

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
 import React from 'react'
-import TodoCounter from './components/TodoCounter';
-import TodoSearch from './components/TodoSearch';
-import TodoList from './components/TodoList';
-import TodoItem from './components/TodoItem';
-import CreateTodoButton from './components/CreateTodoButton';
-const todos = [
-  { text: 'Cortar cebolla', completed: false },
-  { text: 'Tormar el curso de intro a react', completed: false },
-  { text: 'Llorar con la llorona', completed: false }
-];
+import Home from './pages/Home';
 function App() {
   return (
     <>
-      <TodoCounter />
-      <TodoSearch />
-      <TodoList>
-        {todos.map(todo => (
-          <TodoItem key={todo.text} text="{todo.text}" />
-        ))}
-      </TodoList>
-      <CreateTodoButton />
+      <Home/>
     </>
   );
 }
