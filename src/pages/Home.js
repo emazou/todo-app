da4884005cae8d8d81f65f7d673941b22063819d
emazou <estefania.mazou@gmail.com>
2022-08-20 16:01:15 -0500

    components imported

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,34 @@
 import React from 'react'
-import TodoCounter from './components/TodoCounter';
-import TodoSearch from './components/TodoSearch';
-import TodoList from './components/TodoList';
-import TodoItem from './components/TodoItem';
-import CreateTodoButton from './components/CreateTodoButton';
-const todos = [
-    { text: 'Cortar cebolla', completed: false },
-    { text: 'Tormar el curso de intro a react', completed: false },
-    { text: 'Llorar con la llorona', completed: false }
-];
+import CreateTodoButton from '../components/CreateTodoButton';
+import TodoCounter from '../components/TodoCounter';
+import TodoSearch from '../components/TodoSearch';
+import TodoList from '../components/TodoList';
+import TodoItem from '../components/TodoItem';
+import '../styles/Home.css'
+
 export default function () {
+    const todos = [
+        { text: 'Cortar cebolla', completed: true },
+        { text: 'Tormar el curso de intro a react', completed: false },
+        { text: 'Llorar con la llorona', completed: false }
+    ];
     return (
-        <div>
-            <TodoCounter />
-            <TodoSearch />
-            <TodoList>
-                {todos.map(todo => (
-                    <TodoItem key={todo.text} text="{todo.text}" />
-                ))}
-            </TodoList>
-            <CreateTodoButton />
+        <div className='App-container'>
+            <div className='App-todo'>
+                <TodoCounter />
+                <TodoSearch />
+                <TodoList>
+                    {todos.map(todo => (
+                        <TodoItem 
+                        key={todo.text}
+                        text={todo.text}
+                        completed={todo.completed}
+                        />
+                    ))}
+                </TodoList>
+                <CreateTodoButton />
+            </div>
+            
         </div>
     )
 }
