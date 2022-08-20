26cb0bc024e2ba780108b4b210618a224adbdd8c
emazou <estefania.mazou@gmail.com>
2022-08-19 21:15:04 -0500

    home page created

---
+++ src/pages/Home.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import TodoCounter from './components/TodoCounter';
+import TodoSearch from './components/TodoSearch';
+import TodoList from './components/TodoList';
+import TodoItem from './components/TodoItem';
+import CreateTodoButton from './components/CreateTodoButton';
+const todos = [
+    { text: 'Cortar cebolla', completed: false },
+    { text: 'Tormar el curso de intro a react', completed: false },
+    { text: 'Llorar con la llorona', completed: false }
+];
+export default function () {
+    return (
+        <div>
+            <TodoCounter />
+            <TodoSearch />
+            <TodoList>
+                {todos.map(todo => (
+                    <TodoItem key={todo.text} text="{todo.text}" />
+                ))}
+            </TodoList>
+            <CreateTodoButton />
+        </div>
+    )
+}
