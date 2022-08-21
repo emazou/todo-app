8dea616ef73459de9773dd9b4747a7637497f797
emazou <estefania.mazou@gmail.com>
2022-08-20 22:59:23 -0500

    comments deleted

--- src/pages/App.js
+++ src/pages/App.js
@@ -4,39 +4,32 @@ import TodoCounter from '../components/TodoCounter';
 import TodoSearch from '../components/TodoSearch';
 import TodoList from '../components/TodoList';
 import TodoItem from '../components/TodoItem';
-import { TodoContext} from '../TodoContext/TodoContext';
+import { TodoContext } from '../TodoContext/TodoContext';
 import '../styles/App.css';
 
 export default function App() {
-  const {error,loading,searchedTodos,completeTodo, deleteTodo} = React.useContext(TodoContext)
+  const { error, loading, searchedTodos, completeTodo, deleteTodo } = React.useContext(TodoContext)
   return (
-    
-      <div className='App-container'>
-        <div className='App-todo'>
-          <TodoCounter/>{/* 
-            total={totalTodos}
-            completed={completedTodos} */}
-          
-          <TodoSearch/>
-           {/*  searchValue={searchValue}
-            setSearchValue={setSearchValue} */}
-          
-              <TodoList>
-              {error && <p>Hubo un error</p>}
-              {loading && <p>Loading...</p>}
-              {(!loading && !searchedTodos.length) && <p>Crea tu primer todo </p>}
-              {searchedTodos.map(todo => (
-                <TodoItem
-                  key={todo.text}
-                  text={todo.text}
-                  completed={todo.completed}
-                  onComplete={() => completeTodo(todo.text)}
-                  onDelete={() => deleteTodo(todo.text)}
-                />
-              ))}
-            </TodoList>
-          <CreateTodoButton />
-        </div>
+    <div className='App-container'>
+      <div className='App-todo'>
+        <TodoCounter />
+        <TodoSearch />
+        <TodoList>
+          {error && <p>Hubo un error</p>}
+          {loading && <p>Loading...</p>}
+          {(!loading && !searchedTodos.length) && <p>Crea tu primer todo </p>}
+          {searchedTodos.map(todo => (
+            <TodoItem
+              key={todo.text}
+              text={todo.text}
+              completed={todo.completed}
+              onComplete={() => completeTodo(todo.text)}
+              onDelete={() => deleteTodo(todo.text)}
+            />
+          ))}
+        </TodoList>
+        <CreateTodoButton />
       </div>
+    </div>
   )
 }
