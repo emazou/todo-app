0da4f4e9b69f92772c76afcc7ad087e4ce0fa3f7
emazou <estefania.mazou@gmail.com>
2022-08-21 16:46:28 -0500

    TodoSttaus component created

---
+++ src/components/TodoStatus.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../styles/TodoStatus.css'
+export default function TodoStatus() {
+  return (
+    <>
+    <h3 className='TodoStatus'>Ups, no hay tareas</h3>
+    </>
+  )
+}
