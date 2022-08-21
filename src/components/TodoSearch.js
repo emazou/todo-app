e276ee4ca47d9149c3e69f10d64635cfc3b8f940
emazou <estefania.mazou@gmail.com>
2022-08-20 22:52:20 -0500

    added events

--- src/components/TodoSearch.js
+++ src/components/TodoSearch.js
@@ -1,9 +1,20 @@
-import React from 'react'
-import '../styles/TodoSearch.css'
+import React, { useContext } from 'react';
+import '../styles/TodoSearch.css';
+import { TodoContext } from '../TodoContext/TodoContext';
 export default function TodoSearch() {
+  const { searchValue, setSearchValue } = useContext(TodoContext)
+  
+  const onSearchValueChange = (event) =>{
+    setSearchValue(event.target.value);
+  };
   return (
     <>
-      <input className='TodoSearch' placeholder='Buscar tarea' />
+      <input 
+      className='TodoSearch' 
+      placeholder='Buscar TODO' 
+      value={searchValue}
+      onChange={onSearchValueChange}
+      />
     </>
   );
 }
