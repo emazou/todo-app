05f247b824ddab3e04a86d134e2974a6715ec1be
emazou <estefania.mazou@gmail.com>
2022-08-25 22:55:35 -0500

    component modified

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import '../styles/TodoList.css'
 export default function TodoList(props) {
+    const render = props.children
     return (
-        <>
-            <section className='TodoList-container'>
-                <ul>
-                    {props.children}
-                </ul>
-            </section>
-        </>
+        <section className='TodoList-container'>
+            {props.error && props.onError()}
+            {props.loading && props.onLoading()}
+            {(!props.loading && !props.totalTodos) && props.onEmptyTodos()}
+            {(!!props.totalTodos && !props.searchedTodos.length) && props.onEmptySearchResults(props.searchValue)}
+            <ul>
+                {props.searchedTodos.map(render)}
+            </ul>
+        </section>
     )
 }
